ed59305826d7108b4cc62eea33d6b9175e8f26ea
Sylvain Desbureaux <sylvain.desbureaux@orange.com>
2013-01-31 18:12:41 +0100

    add attr_accessible attributes on Metric model

--- app/models/system_metrics/metric.rb
+++ app/models/system_metrics/metric.rb
@@ -1,5 +1,6 @@
 module SystemMetrics
   class Metric < ActiveRecord::Base
+    attr_accessible :name, :payload, :time, :transaction_id, :end, :duration, :action, :category, :children
 
     set_table_name 'system_metrics'
     has_many :children, :class_name => self.name, :foreign_key => :parent_id
